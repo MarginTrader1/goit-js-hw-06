fd5cf3d74beb8940f2e2af3f6727025820076b4b
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-03-27 00:10:34 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,14 @@
-const liElem = document.querySelector("#categories");
+console.log(document);
+console.dir(document);
+
+const liElem = document.querySelector(".item li");
 console.log(liElem);
 
-const liElemList = document.querySelectorAll(".item");
+const liElemList = document.querySelectorAll(".item li");
 console.log(liElemList)
 console.log(`Number of categories:`, liElemList.length);
+console.log(`One of main:`, liElemList[4]);
+
+
+
+

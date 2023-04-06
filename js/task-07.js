7e1cb3a2f7883c8b3a7eaf864550a502bd3752a1
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-04-06 22:38:15 +0300

    8 work

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,19 @@
 const inputEl = document.querySelector(`#font-size-control`);
 const spanEl = document.querySelector(`#text`);
 
+
 console.log(inputEl);
-console.dir(spanEl);
+console.log(spanEl);
+
+inputEl.addEventListener(`input`, onInput)
+
+function onInput (event) {
+    console.log('Это инпут');
+    spanEl.classList.add(`item`);
+    
+    console.log(event.currentTarget.value);
+    spanEl.style.fontSize = `${event.currentTarget.value}px`;
+    console.log(spanEl.style.fontSize);
+
+}
+
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
 
+const formElem = document.querySelector(".login-form");
+
+
+formElem.addEventListener(`submit`, handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+
+      if (email.value === "" || password.value === "") {
+        return alert(`Все поля должны быть заполнены`);
+      }
+    
+      console.log(`email: ${email.value}, Password: ${password.value}`);
+      event.currentTarget.reset();
+
+}

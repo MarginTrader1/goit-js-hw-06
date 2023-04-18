14aaeda9d3b4d6dd2e5f562926c28a8a9f4f0bb8
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-04-18 21:09:44 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonElem = document.querySelector(".change-color");
+const pElem = document.querySelector(".color");
+const bodyElem = document.querySelector("body");
+
+
+buttonElem.addEventListener(`click`, handleClick);
+
+
+function handleClick(event) {
+      
+      const randomColor = getRandomHexColor();
+      pElem.textContent = randomColor;
+      bodyElem.style.backgroundColor = randomColor;
+}

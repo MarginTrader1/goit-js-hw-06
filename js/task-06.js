ef9e01150efa0d83e5561f71c60f71c7f085cb96
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-04-19 21:52:17 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -5,12 +5,17 @@ console.log(inputEl.value);
 inputEl.addEventListener(`blur`, onInputBlur);
 
 function onInputBlur (event) {
-    console.log('Это пропал блюр');
+    
     console.log(inputEl.dataset.length);
     console.log(event.currentTarget.value.length);
     console.log(event.currentTarget.value);
 
-    event.currentTarget.value.length < inputEl.dataset.length ? 
-    inputEl.classList.add(`invalid`) : 
-    inputEl.classList.add(`valid`);
+    
+    if (event.currentTarget.value.length === Number(inputEl.dataset.length)) {
+        inputEl.classList.add(`valid`)
+    } else {
+    inputEl.classList.add(`invalid`)};
+
 }
+
+
